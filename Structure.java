9a1f14b06ec0b595e80ce959e783baf18c55327a
selma-tiks <97076500+selma-tiks@users.noreply.github.com>
2022-01-19 05:46:04 +0100

    Add files via upload

---
+++ Structure.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travail_pour_tous;
+
+/**
+ *
+ * @author Carré Vilmorin
+ */
+public class Structure {
+    private String nom;
+    private String numero_contribuable;
+    private String localisation;
+    private String Domaine;
+    private String email;
+    private String boite_postale;
+    private String date_de_creation;
+    
+    public Structure(String nom,String numero_contribuable,String localisation,String Domaine,String email,String boite,String date_de_creation)
+    {
+        this.nom=nom;
+    }
+    
+    
+}
