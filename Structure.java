5667ccac09ab4aecdc7826046df7a9c396d14cd8
selma-tiks <97076500+selma-tiks@users.noreply.github.com>
2022-01-20 14:33:25 +0100

    Update Structure.java

--- Structure.java
+++ Structure.java
@@ -5,23 +5,24 @@
  */
 package travail_pour_tous;
 
-/**
- *
- * @author Carré Vilmorin
- */
+
 public class Structure {
     private String nom;
     private String numero_contribuable;
     private String localisation;
-    private String Domaine;
+    private String domaine;
     private String email;
     private String boite_postale;
     private String date_de_creation;
     
-    public Structure(String nom,String numero_contribuable,String localisation,String Domaine,String email,String boite,String date_de_creation)
+    public Structure(String nom,String numero,String localisation,String domaine,String email,String boite,String date)
     {
         this.nom=nom;
+        this.numero_contribuable=numero;
+        this.localisation=localisation;
+        this.domaine=domaine;
+        this.email=email;
+        this.boite_postale=boite;
+        this.date_de_creation=date;
+    
     }
-    
-    
-}
